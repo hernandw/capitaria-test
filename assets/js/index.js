a4d8139b8df4b995c397eca9b7e0230828da024f
hernandw <hernandw@gmail.com>
2022-02-15 11:59:00 -0300

    generación de calendario

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,154 @@ const traerDatos = () => {
      /* console.log(datos.results['0'].picture.large) */
      console.log(datos)
      calendar.innerHTML = `
-<p>Dia: ${datos.monday['0'].name}</p>
+     <div class="container">
+     <div class="horario">
+       <div>
+         <div class="hora">hora</div>
+         <div class="hora">8:00</div>
+         <div class="hora">8:20</div>
+         <div class="hora">8:40</div>
+         <div class="hora">9:00</div>
+         <div class="hora">9:20</div>
+         <div class="hora">9:40</div>
+         <div class="hora">10:00</div>
+         <div class="hora">10:20</div>
+         <div class="hora">10:40</div>
+         <div class="hora">11:00</div>
+         <div class="hora">11:20</div>
+         <div class="hora">11:40</div>
+         <div class="hora">12:00</div>
+       </div>
+       <div>
+         <div class="hora">Lunes</div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div></div>
+       </div>
+       <div>
+         <div class="hora">Martes</div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+       </div>
+       <div>
+         <div class="hora">Miercoles</div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+       </div>
+       <div>
+         <div class="hora">Jueves</div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+       </div>
+       <div>
+         <div class="hora">Viernes</div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+       </div>
+       <div>
+         <div class="hora">Sábado</div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+       </div>
+       <div>
+         <div class="hora">Domingo</div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+         <div class="hora"></div>
+       </div>
+       <div>
+         <div class="hora">hora</div>
+         <div class="hora">8:00</div>
+         <div class="hora">8:20</div>
+         <div class="hora">8:40</div>
+         <div class="hora">9:00</div>
+         <div class="hora">9:20</div>
+         <div class="hora">9:40</div>
+         <div class="hora">10:00</div>
+         <div class="hora">10:20</div>
+         <div class="hora">10:40</div>
+         <div class="hora">11:00</div>
+         <div class="hora">11:20</div>
+         <div class="hora">11:40</div>
+         <div class="hora">12:00</div>
+       </div>
+     </div>
      `
      /* <img src="${datos.results['0'].picture.large}" width="100px"> */
       
